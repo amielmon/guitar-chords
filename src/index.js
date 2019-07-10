b7726f4c78f9aa43e651e11ae87a81cb8fb28f0f
Amiel Monasterial <amiel@Amiels-MacBook-Pro.local>
2019-07-10 17:16:57 -0400

    Initial project setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+
+
+
+
+const resolvers = {}
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+  })
+  server.start(() => console.log(`Server is running on http://localhost:4000`))
