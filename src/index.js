2448eaff89bdaf4e45cb89689c8bf13d28ce364a
Amiel Monasterial <amiel@Amiels-MacBook-Pro.local>
2019-07-17 15:46:57 -0400

    Update Schema

--- src/index.js
+++ src/index.js
@@ -9,5 +9,6 @@ const resolvers = {}
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers,
+    context: { prisma },
   })
   server.start(() => console.log(`Server is running on http://localhost:4000`))
